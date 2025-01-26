dff9660110982e8e7ddad546c56b45e6ac34a250
Babuuum <darckiller@gmail.com>
2025-01-26 20:18:10 +0300

    PoC2

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class QuestType(models.Model):
     Побочные: типовые квесты, задачи на leetcode, hackerrank, простые проекты например: написание калькулятора, боссы, данжы, Разработка.
         Боссы: это реальный "боевой проект", требующий выполнение условий (в виде, ряда побочных заказов) и открывающийся за валюту, очки за прохождение x2
         Данжи: квесты подразумевающие совместное прохождение, требующие затраты огромного количества валюты для открытия, очки за прохождение x5
-        Разработка: квесты по улучшению непосредственно проекта ITachi, можно брать напрямую из to do листа, очки за прохождение x3
+        Разработка: квесты по улучшению непосредственно проекта itachi, можно брать напрямую из to do листа, очки за прохождение x3
     доп:
         *побочные квесты можно разбить на дополнительные типы...
         *так же можно включить заказы фриланса.
@@ -35,7 +35,7 @@ class Quest(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     type = models.ForeignKey(QuestType, on_delete=models.CASCADE, related_name='quests')
-    points = models.IntegerField()
+    points = models.IntegerField() # dymau ly4she sdelat' eto 4ere3 grade
 
     def __str__(self):
         return f"{self.name} ({self.type})"
@@ -77,7 +77,7 @@ class AchievementGrade(models.Model):
 class Achievement(models.Model):
     """
     Основная механника, к которой привязанно по сути все:
-    Каждая Ачивка имеет: Название, Требования, Шаги выполненияm, Награды.
+    Каждая Ачивка имеет: Название, Требования, Шаги выполнения, Награды.
 
     IMHO: не совсем понимаю разницу между Требования и Шаги выполнения -- это одно и то же?
     Плюс я так понял, награды привязаны к грейдам, а не к самим ачивкам.
@@ -93,6 +93,32 @@ class Achievement(models.Model):
         return f"{self.name} ({self.type}) - {self.grade}"
 
 
+class Skill(models.Model):
+    # one to one with quests
+    name = models.CharField(max_length=100)
+
+def __str__(self):
+    return f"{self.name}"
+
+
+class Stack(models.Model):
+    name = models.CharField(max_length=100)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='skill')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Profession(models.Model):
+    # one to one with Skills
+    name = models.CharField(max_length=100)
+    stack = models.ForeignKey(Stack, on_delete=models.CASCADE, related_name='profession') # nyjni dl9 proverki trebovanii kvestov
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='profession')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class UserAchievement(models.Model):
     # user's completed achievements
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='achievements')
@@ -105,3 +131,40 @@ class UserQuest(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='quests')
     quest = models.ForeignKey(Quest, on_delete=models.CASCADE, related_name='user_quests')
     date_completed = models.DateTimeField(auto_now_add=True)
+
+
+class UserProfession(models.Model):
+    #many to many user + QuestsTags
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='skills')
+    quest = models.ForeignKey(Profession, on_delete=models.CASCADE, related_name='user_skills')
+
+
+class UserSkill(models.Model):
+    #many to many user + QuestsTags
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='skills')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='user_skills')
+
+
+class QuestsSkills(models.Model):
+    pass
+
+
+class QuestsProfession(models.Model):
+    pass
+
+
+#class QuestsTags(models.Model):
+    #Необходимы только для разблокировки квестов.
+
+
+#class AchievementsTags(models.Model):
+    #Необходимы для взаимодействия с ачивками(только для админского взаимодействия с ачивками)
+
+
+    """
+    Так как у нас не на прямую скиллы открывают профы, а в каждой профе есть свои уникальные категории, которые уже содержат скиллы
+    мложно это реализовать поэтому предлагаю, сделать так: вынести все Profession -> requirements -> skills, в отдельный файл, доставать их
+    из него и проверять в функции/методе
+    """
+
+#vopros, sv93i kvestov, usera i navikov prof
