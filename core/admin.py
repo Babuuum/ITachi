ba33a8d0c35ecfafa6ec259503c77bd73d7b54f3
EeOneDown <ione.platonov@yandex.ru>
2025-02-02 18:04:32 -0500

    models + admin

--- core/admin.py
+++ core/admin.py
@@ -1,22 +1,79 @@
 from django.contrib import admin
-
-from .models import Achievement, AchievementType, Quest, QuestType, UserAchievement, AchievementGrade, UserQuest
-
-
-class AchievementInline(admin.TabularInline):
-    model = Achievement
-    extra = 1
-    fields = ('name', 'grade', 'type')
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import AchievementType, AchievementGrade, Achievement, UserAchievement
 
 
-class QuestAdmin(admin.ModelAdmin):
-    inlines = [AchievementInline]
+class UserAchievementInline(admin.TabularInline):
+    def has_proof(self, obj) -> bool:
+        return bool(obj.description)
+
+    has_proof.short_description = "Has Proof"
+    has_proof.boolean = True
+
+    model = UserAchievement
+    extra = 0  # Do not add empty forms
+    fields = ("achievement", "date_completed", "has_proof")
+    readonly_fields = ("achievement", "date_completed", "has_proof")
 
 
-admin.site.register(Quest, QuestAdmin)
-admin.site.register(QuestType)
-admin.site.register(Achievement)
-admin.site.register(AchievementType)
-admin.site.register(AchievementGrade)
-admin.site.register(UserAchievement)
-admin.site.register(UserQuest)
+class UserAdmin(DefaultUserAdmin):
+    inlines = [UserAchievementInline]
+
+    def total_score(self, obj):
+        return sum(ua.achievement.grade.points for ua in obj.achievements.all())
+
+    total_score.short_description = "Total Score"
+
+    def total_achievements(self, obj):
+        return obj.achievements.count()
+
+    total_achievements.short_description = "Total Achievements"
+
+    readonly_fields = ("total_score", "total_achievements")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),  # from DefaultUserAdmin.fieldsets
+        ("Achievements", {"fields": ("total_score", "total_achievements")}),
+    )
+    list_display = ("username", "total_score", "total_achievements")
+    list_filter = ()
+    search_fields = ("username", "first_name", "last_name", "email")
+    filter_horizontal = ()
+
+
+# Unregister default User admin and register the customized one
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(AchievementType)
+class AchievementTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(AchievementGrade)
+class AchievementGradeAdmin(admin.ModelAdmin):
+    list_display = ("name", "points")
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "grade")
+
+
+@admin.register(UserAchievement)
+class UserAchievementAdmin(admin.ModelAdmin):
+    def has_proof(self, obj) -> bool:
+        return bool(obj.description)
+
+    has_proof.short_description = "Has Proof"
+    has_proof.boolean = True
+
+    search_fields = (
+        "user__username",
+        "achievement__name",
+        "achievement__description",
+        "achievement__type__name",
+        "achievement__grade__name",
+    )
+    list_display = ("user", "achievement", "date_completed", "has_proof")
